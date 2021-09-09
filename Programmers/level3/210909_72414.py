5c358b5cb33a19b40a8af787a14cea1a698f886a
KoYeJoon <yejoon08@ajou.ac.kr>
2021-09-10 01:42:17 +0900

    210909 프로그래머스 3단계 1문제 해결

--- Programmers/level3/210909_72414.py
+++ Programmers/level3/210909_72414.py
@@ -2,7 +2,6 @@ def solution(play_time, adv_time, logs):
     if play_time == adv_time :
         return '00:00:00'
 
-    answer = ''
     # convert hour ,minute to second
     play_time_list = play_time.split(':')
     play_time_second = int(play_time_list[0])*60*60 + int(play_time_list[1])*60 + int(play_time_list[2])
@@ -11,7 +10,7 @@ def solution(play_time, adv_time, logs):
     adv_time_second = int(adv_time_list[0])*60*60 + int(adv_time_list[1])*60 + int(adv_time_list[2])
 
     # time, people 변화
-    time_stamp= []
+    dp_time_people_stamp= [0 for i in range(play_time_second+1)]
     for i in logs :
         temp = i.split('-')
         start_time_list = temp[0].split(':')
@@ -20,51 +19,30 @@ def solution(play_time, adv_time, logs):
         start_time_second = int(start_time_list[0])*60*60 + int(start_time_list[1])*60 + int(start_time_list[2])
         end_time_second = int(end_time_list[0])*60*60 + int(end_time_list[1])*60 + int(end_time_list[2])
 
-        time_stamp.append([start_time_second,1])
-        time_stamp.append([end_time_second,-1])
+        dp_time_people_stamp[start_time_second]+=1
+        dp_time_people_stamp[end_time_second]+=-1
 
-    time_stamp.sort()
+    for i in range(1,play_time_second+1):
+        dp_time_people_stamp[i] += dp_time_peoㄴple_stamp[i-1]
 
-    n = 1
-    accum_dict = {}
-    temp_start = time_stamp[0][0]
-    for i in range(1,len(time_stamp)):
-        accum_dict[temp_start] = n * (time_stamp[i][0]-temp_start)
-        print(temp_start,n * (time_stamp[i][0]-temp_start))
-        temp_start = time_stamp[i][0]
-        n += time_stamp[i][1]
+    for i in range(1,play_time_second+1):
+        dp_time_people_stamp[i] += dp_time_people_stamp[i-1]
 
-
-
-
-    accum_dict_items = sorted(list(accum_dict.items()))
-
-    s = 0
-    answer_time = 0
+    max_people = dp_time_people_stamp[adv_time_second]
     max_accum_time = 0
-    while s < len(accum_dict_items):
-        temp_time = 0
-        end_time = accum_dict_items[s][0] + adv_time_second
-        for i in range(s+1,len(accum_dict_items)):
-            if end_time > accum_dict_items[i][0] :
-                temp_time += accum_dict_items[i-1][1]
-            else :
-                if accum_dict_items[i][0] == end_time :
-                    temp_time += accum_dict_items[i][1]
-                else :
-                    temp_time += accum_dict_items[i][1] // (accum_dict_items[i][0]-end_time)
-                break
-        if max_accum_time < temp_time :
-            max_accum_time = temp_time
-            answer_time = accum_dict_items[s][0]
+    for start_point in range(play_time_second-adv_time_second+1):
+        if max_people < dp_time_people_stamp[start_point+adv_time_second]-dp_time_people_stamp[start_point] :
+            max_people = dp_time_people_stamp[start_point+adv_time_second] - dp_time_people_stamp[start_point]
+            max_accum_time = start_point+1
 
-        s += 1
 
-    answer = str(answer_time//3600).zfill(2) + ":"
-    answer_time -= (answer_time//3600) * 3600
-    answer += str(answer_time//60).zfill(2) + ":"
-    answer_time -= (answer_time//60) * 60
-    answer += str(answer_time%60).zfill(2)
-    return answer
+    hours = max_accum_time // 3600
+    mins = (max_accum_time % 3600) // 60
+    secs = max_accum_time % 60
+    return '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
 
-print(solution("02:03:55","00:14:15",["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]))
+
+
+
+
+print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:25:50-00:48:29", "00:40:31-01:00:00", "01:37:44-02:02:30", "01:30:59-01:53:29"]))
