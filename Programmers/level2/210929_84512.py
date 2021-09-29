53a5d91413f004c012d3d531ae1fc36d000fe930
KoYeJoon <yejoon08@ajou.ac.kr>
2021-09-29 23:59:05 +0900

    210929 프로그래머스 2단계 한문제

--- Programmers/level2/210929_84512.py
+++ Programmers/level2/210929_84512.py
@@ -3,12 +3,7 @@ def solution(word):
     vowel = {'A':1,'E':2,'I':3,'O':4,'U':5}
     number = {0:780, 1:155, 2:30, 3:5,4:0}
 
-    new_word = word.rjust(5,'Z')
-    idx=0
-    while new_word[idx] == 'Z' :
-        idx += 1
-
-    for i in range(5-idx) :
+    for i in range(len(word)) :
         if word[i]=='A' :
             answer += 1
         else :
