981a16445bc361bf57052c0e594f2a5bf0c775be
KoYeJoon <yejoon08@ajou.ac.kr>
2021-11-01 23:38:08 +0900

    211101 python docktyping 정리

---
+++ DataStructure/CS/ducktyping.py
@@ -0,0 +1,25 @@
+class Parrot:
+    def fly(self):
+        print("Parrot flying")
+
+class Airplane:
+    def fly(self):
+        print("Airplane flying")
+
+class Whale:
+    def swim(self):
+        print("Whale swimming")
+
+def lift_off(entity):
+    entity.fly()
+
+parrot = Parrot()
+airplane = Airplane()
+whale = Whale()
+
+try :
+    lift_off(parrot) # prints `Parrot flying`
+    lift_off(airplane) # prints `Airplane flying`
+    lift_off(whale) # Throws the error `'Whale' object has no attribute 'fly'
+except Exception as e :
+    print(e)
